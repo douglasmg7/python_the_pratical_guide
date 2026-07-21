6d5578599856ba65075a84e129d6e28b7fa4d98c
douglasmg7 <douglasmg7@gmail.com>
2026-07-21 15:09:33 -0300

    blockchain script refactor

--- blockchain.py
+++ blockchain.py
@@ -2,18 +2,38 @@
 blockchain = []
 
 def get_last_blockchain_value():
+    if len(blockchain) == 0:
+        return None
     return blockchain[-1]
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_value(transaction_amount, last_transaction):
+    if last_transaction is None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
-    #  print(blockchain)
+
 
 def get_user_input():
-    return float(input('Transaction amount: '))
+    print('\n1: Add transaction')
+    print('2: Print transaction')
+    print('Enter: Exit')
+    return input('Your choice: ')
+
+def print_blockchain():
+    for index, transaction in enumerate(blockchain):
+        print(f'{index} - {transaction}')
 
 
-add_value(get_user_input())
-add_value(get_user_input(), get_last_blockchain_value())
-add_value(get_user_input(), get_last_blockchain_value())
-
-print(blockchain)
+while True:
+    choice = get_user_input()
+    match choice:
+        case '':
+            print('Done')
+            break
+        case '1':
+            tx_amount = float(input('Transaction amount: '))
+            add_value(tx_amount, get_last_blockchain_value())
+        case '2':
+            print_blockchain()
+        case _:
+            print('Invalid choice')
+            continue
